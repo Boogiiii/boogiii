58d65509b0b2c8e5b2678133a7123e821c4973fc
Boogiiii <100068980+Boogiiii@users.noreply.github.com>
2022-03-07 01:20:04 +0800

    shine file

---
+++ labmap.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ 
+class lbmap {
+ 
+    public static void main(String args[])
+    {
+        Map<String, Integer> lb= new HashMap<String, Integer>();
+        lb.put("a ", new Integer(25));
+        lb.put("b ", new Integer(20));
+        
+        for (Map.Entry<String, Integer> me : lb.entrySet()) {
+            System.out.print(me.getKey() + ":" + me.getValue());
+        }
+    }
+}
